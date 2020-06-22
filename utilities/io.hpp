78194865c9d5b8f89f5ceddeb2fe54f273fb80d7
Matthew Larkins <larkins.07@gmail.com>
2020-06-22 05:50:41 +0000

    Deleting everything to do a clean upload of the project with a new organization scheme

--- utilities/io.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-void read_image_file_to_vector(std::string file_name, std::vector<uint8_t> *data);
-void write_data_vector_to_file( std::string file_name, std::vector<uint8_t> *data, const int WIDTH, const int HEIGHT );
-
-#endif
